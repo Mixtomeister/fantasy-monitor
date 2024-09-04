50cc2032107a84d7879d131cd10e26118068b0aa
Iván Gálvez <ivan.galvez.rochel@gmail.com>
2024-09-04 14:48:51 +0200

    Change models fields

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -12,28 +12,34 @@ class PlayerTeamListFilter(admin.SimpleListFilter):
             yield (team.id, team.name)
 
     def queryset(self, request, queryset):
-        print(self.value())
+        if self.value():
+            player_ids = Player.objects.filter(
+                playerupdate__team_id=self.value()
+            ).distinct().values_list('id', flat=True)
+
+            return queryset.filter(id__in=player_ids)
 
 class TeamAdmin(admin.ModelAdmin):
-    search_fields = ('name', 'shortName', 'teamId')
+    search_fields = ('name', 'short_name', 'team_api_id')
 
 class PlayerAdmin(admin.ModelAdmin):
     list_filter = [PlayerTeamListFilter]
-    search_fields = ('name', 'shortName', 'playerId')
+    search_fields = ('name', 'short_name', 'player_api_id')
 
 class PlayerUpdateAdmin(admin.ModelAdmin):
     list_display = ('player_name_field', 'team', 'position', 'date', 'market_value', 'status')
     list_filter = ('team__name', 'position', 'date', 'status')
-    search_fields = ('player__name', 'player__shortName', 'player__playerId')
+    search_fields = ('player__name', 'player__short_name', 'player__player_api_id')
 
     def player_name_field(self, obj):
-        return f'{obj.player.name} ({obj.player.shortName})'
+        return f'{obj.player.name} ({obj.player.short_name})'
     player_name_field.allow_tags = True
     player_name_field.short_description = "Player Name"
 
 class PlayerPointsAdmin(admin.ModelAdmin):
-    list_display = ('player', 'round', 'points', 'idealXI')
-    list_filter = ['round', 'idealXI']
+    list_display = ('player', 'round', 'points', 'ideal_xi')
+    list_filter = ['round', 'ideal_xi']
+    search_fields = ["player__name"]
 
 # Register your models here.
 admin.site.register(Team, TeamAdmin)
--- fantasy/models.py
+++ fantasy/models.py
@@ -2,20 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Team(models.Model):
-    teamId = models.IntegerField()
+    team_api_id = models.IntegerField()
     name = models.CharField(max_length=32)
-    shortName = models.CharField(max_length=3)
+    short_name = models.CharField(max_length=3)
     slug = models.SlugField(max_length=32)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 class Player(models.Model):
-    playerId = models.IntegerField()
+    player_api_id = models.IntegerField()
     name = models.CharField(max_length=64)
-    shortName = models.CharField(max_length=32)
+    short_name = models.CharField(max_length=32)
     slug = models.SlugField(max_length=32)
-    lastSeasonPoints = models.IntegerField()
+    last_season_points = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def team(self):
@@ -69,9 +73,11 @@ class PlayerUpdate(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.RESTRICT, null=True)
     position = models.IntegerField(choices=PlayerPosition)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField()
     market_value = models.IntegerField()
     status = models.CharField(max_length=12, choices=PlayerStatus)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.date.strftime('%Y-%m-%d')}] [{self.team.name}] {self.player.name}'
@@ -83,7 +89,9 @@ class PlayerPoints(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     round = models.IntegerField()
     points = models.IntegerField()
-    idealXI = models.BooleanField()
+    ideal_xi = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[Week {self.round}] {self.player.name} ({self.points})'
